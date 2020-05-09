d497960689d8f6bde40bfbb599ba42cc14a24912
Christopher Zuniga <c.zuniga21@gmail.com>
2020-05-09 15:37:42 -0600

    Clase 2: Agregando herencia Persona - Trabajador
    Se agregó la clase Trabajador.

---
+++ CursoCSharp/Entity/Models/Trabajador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Models
+{
+    public class Trabajador : Persona
+    {
+        public string Empresa;
+        public string Cargo;
+        public double Sueldo;
+
+        public Trabajador()
+        {
+
+        }
+
+        public Trabajador(string pE, string pC, double pS)
+        {
+            this.Empresa = pE;
+            this.Cargo = pC;
+            this.Sueldo = pS;
+        }
+
+
+        public Trabajador(string I, string N, int E, string S,
+            string pE, string pC, double pS) : base(I, N, E, S)
+        {
+            this.Empresa = pE;
+            this.Cargo = pC;
+            this.Sueldo = pS;
+        }
+
+        public virtual void MostrarInfo()
+        {
+            base.MostrarInfo();
+            Console.WriteLine("{0}{1}", "Empresa: ", this.Empresa);
+            Console.WriteLine("{0}{1}", "Cargo: ", this.Cargo);
+            Console.WriteLine("{0}{1}", "Sueldo: ", this.Sueldo);
+        }
+
+    }
+}
