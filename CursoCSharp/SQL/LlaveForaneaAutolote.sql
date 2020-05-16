08fb052bb9e908497fae418a6367ec256209f919
Christopher Zuniga <c.zuniga21@gmail.com>
2020-05-16 16:50:08 -0600

    Clase 3 :  Modelando base de datos Autolote

---
+++ CursoCSharp/SQL/LlaveForaneaAutolote.sql
@@ -0,0 +1,11 @@
+use Autolote
+go
+
+--alter table t_Agente add foreign key (IdBase) references t_Base(IdBase)
+--alter table t_Mecanico add foreign key (idBase) references t_Base(IdBase)
+--alter table t_Reparacion add foreign key (idMecanico) references t_Mecanico(idMecanico)
+--alter table t_Reparacion add foreign key (idAuto) references t_Auto(idAuto)
+--alter table t_Auto add foreign key (idBase) references t_Base(IdBase)
+--alter table t_Orden add foreign key(idAuto) references t_Auto(idAuto)
+--alter table t_Orden add foreign key(idCliente) references t_Cliente(idCliente)
+
