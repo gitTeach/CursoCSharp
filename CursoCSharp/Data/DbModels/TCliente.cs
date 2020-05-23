1575cfc99d403a3511feb17cfac4ed52c7384c31
Christopher Zuniga <c.zuniga21@gmail.com>
2020-05-23 15:12:27 -0600

    Clase 4 : Agregando Modelo de Base de Datos con EF Core
    Archivo Faltante.

---
+++ CursoCSharp/Data/DbModels/TCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.DbModels
+{
+    public partial class TCliente
+    {
+        public TCliente()
+        {
+            TOrden = new HashSet<TOrden>();
+        }
+
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NumeroTelefono { get; set; }
+        public string Direccion { get; set; }
+
+        public virtual ICollection<TOrden> TOrden { get; set; }
+    }
+}
